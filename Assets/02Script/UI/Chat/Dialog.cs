6d7a323e0a90ac1a2cb512de33d956c449b6f8f2
neeeeding <abc01096780866@gmail.com>
2025-04-02 09:58:24 +0900

    Add SelectBtn

--- Assets/02Script/UI/Chat/Dialog.cs
+++ Assets/02Script/UI/Chat/Dialog.cs
@@ -14,7 +14,7 @@ public class Dialog : MonoBehaviour
     [Header("Component")]
     [SerializeField] private ChatSetting setting; //세팅 해주는 거
     [SerializeField] private TextMeshProUGUI dialogText; //대화
-    [SerializeField] private TextMeshProUGUI[] selectTexts; //선택지 대화
+    [SerializeField] private SelectBtn[] selectTexts; //선택지 대화
     [Space(20f)]
     [SerializeField] private CharacterSO[] allCharacter; //모든 캐릭터의 정보. (호감도 못 올림)
     [Space(50f)]
@@ -41,18 +41,6 @@ public class Dialog : MonoBehaviour
         DoChat();
     }
 
-    public void ClickNext() //다음으로
-    {
-        if(!HaveSelect(currentChat))
-        {
-            if (DoChat())
-            {
-                UISettingManager.Instance.CloseChat();
-                OnGame?.Invoke();
-            }
-        }
-    }
-
     private void GetDialog() //대화 (챕터 번호) 얻기. (List)
     {
         TextAsset currentDialog = currentSO.characterDialog[0];
@@ -69,6 +57,33 @@ public class Dialog : MonoBehaviour
                 break;
             }
         }
+    }
+
+    public void ClickNext() //다음으로
+    {
+        if(!HaveSelect(currentChat))
+        {
+            if (DoChat())
+            {
+                UISettingManager.Instance.CloseChat();
+                OnGame?.Invoke();
+            }
+        }
+    }
+
+    private void SelectChat(int selectNum) //선택된 선택지가 있을 시 (대화 계속 진행)
+    {
+        currentChat += selectNum - currentNum; //선택된 번호로 바꿔주기
+        currentNum = selectNum;
+
+        //다음 번호로 바꿔 주기 (선택된 문항을 말할 순 없으니까. (사실 호불호긴 해.))
+        int nextNum = (int)dialog[currentChat][DialogType.NextNum.ToString()];
+        currentChat += nextNum - currentNum == 0 ?
+            +1 : nextNum - currentNum; //다음 번호 정해주기. (마지막이 본인이면 1추가로 나가게 해버리기.)
+        currentNum = nextNum;
+
+        OffSelectText(); //다 꺼주기
+        DoChat();
     }
 
     private void IsHoldItem() //들고 있는 아이템 있다면 (챕터 번호)
@@ -88,25 +103,54 @@ public class Dialog : MonoBehaviour
         }
     }
 
-    private bool DoChat() //대화를 위한 배열 돌기
+    private bool DoChat() //대화(실질적인 랜더러)
     {
         bool getOut = true;
 
         //대화가 존재하는지 (배열 확인)
-        if (dialog[currentChapter][DialogType.Chapter.ToString()].ToString() == currentChapter.ToString()
-                       && dialog[currentChapter][DialogType.Num.ToString()].ToString() == currentNum.ToString()) //해당 배열의 수가 챕터랑 번호가 같으면
+        if (dialog[currentChat][DialogType.Chapter.ToString()].ToString() == currentChapter.ToString()
+                       && dialog[currentChat][DialogType.Num.ToString()].ToString() == currentNum.ToString()) //해당 배열의 수가 챕터랑 번호가 같으면
         {
             getOut = false;
-            LoveUP(currentNum);
+            LoveUP(currentChat);
         }
         else
             getOut = true;
 
-
         setting.CurrentCharacter(currentSO); //세팅 해주는 거
-        dialogText.text = dialog[currentChat][DialogType.Text.ToString()].ToString(); // 채팅 대화 입력하기
-        currentNum++;
+        string chatText = IsExchangeText(dialog[currentChat][DialogType.Text.ToString()].ToString(), "`", ","); //변환 해주고 원했던 대화
+        dialogText.text = chatText;
+
+        if (dialog[currentChat][DialogType.NextNum.ToString()].ToString() != "") // 다음 번호가 안 비어 있다면.
+        {
+            int nextNum = (int)dialog[currentChat][DialogType.NextNum.ToString()] - 1;
+            currentChat += nextNum - currentNum == 0 ?
+                +1 : nextNum - currentNum; //다음 번호 정해주기. (마지막이 본인이면 1추가로 나가게 해버리기.(대화 자체는 줄어버림.(???)))
+            currentNum = nextNum + 1;
+        }
         return getOut;
+    }
+
+    private string IsExchangeText(string dial, string text, string change) //어떤 문자를 다른 문자로 바꾸기 ( ` => , )
+    {
+        string changeDialog = "";
+
+        if(dial.Contains(text)) //바꿀것이 포함 되어있는지
+        {
+            string[] saveText = dial.Split(text); //일단 쪼개주기
+            for(int i = 0; i < saveText.Length -1; i++) //마지막은 미포함이니 빼주고 반복
+            {
+                changeDialog += saveText[i];
+                changeDialog += change;
+            }
+            changeDialog += saveText[saveText.Length - 1]; //빼줬던 마지막 추가 해주기
+        }
+        else //없으면 그대로
+        {
+            changeDialog = dial;
+        }
+
+        return changeDialog;
     }
 
     private void LoveUP(int i) //호감도 오르거나 내리는 거 있으면 해주기.
@@ -122,11 +166,11 @@ public class Dialog : MonoBehaviour
     {
         foreach (var item in selectTexts)
         {
-            item.gameObject.transform.parent.gameObject.SetActive(false);
+            item.gameObject.SetActive(false);
         }
     }
 
-    private bool HaveSelect(int i) //선택지가 있는지 (있으면 개수만 큼 켜줌.)
+    private bool HaveSelect(int i) //선택지가 있는지 (있으면 개수만 큼 세팅.)
     {
         bool haveSelec = false; //false : 선택지 없음 (다음 번호)/ true : 선택지 있음 (건너뛰어서)
 
@@ -138,35 +182,34 @@ public class Dialog : MonoBehaviour
             int count = (int)dialog[i][DialogType.Select.ToString()];
             for(int j = 0; j < count && j < selectTexts.Length; j++) //반복문
             {
-                selectTexts[j].gameObject.transform.parent.gameObject.SetActive(true);
+                selectTexts[j].gameObject.SetActive(true);
 
-                int selectNumText = (int)dialog[i][DialogType.SelectText.ToString() + (j + 1)] - 1; //선택지에 사용될 텍스트 (번호) 찾기
+                int selectNumText = (int)dialog[i][DialogType.SelectText.ToString() + (j + 1)] - 1/*0부터 시작하니*/; //선택지에 사용될 텍스트 (번호) 찾기
+                string text = dialog[selectNumText][DialogType.Text.ToString()].ToString();
 
-                selectTexts[j].text = dialog[selectNumText][DialogType.Text.ToString()].ToString(); //해당 번째의 텍스트 입력
+                selectTexts[j].SetSelect(text, selectNumText); //선택 그거 세팅 해주기.
             }
         }
 
         return haveSelec;
     }
 
-    private T GetEnum<T>(/*out*/ T wantEnum, string value) where T : struct, Enum //enum 얻는거
+    #region EnDi
+    private void OnEnable()
     {
-        //if( Enum.TryParse(value, out wantEnum)) //존재 하는지
-        //{
-            return (T)Enum.Parse(typeof(T), value);
-        //}
-        //else
-        //{
-        //    return ;
-        //}
-
-        //return Enum.TryParse(value, out wantEnum);
+        SelectBtn.OnSelect += SelectChat;
     }
+
+    private void OnDisable()
+    {
+        SelectBtn.OnSelect -= SelectChat;
+    }
+    #endregion
 }
 
 public enum DialogType
 {
-        Select, //선택지 인지
+        Select, //선택지 인지 (개수)
         ItemCategory, //아이템 카테고리
         Item, //아이템 종류
         GetLove, //얻는 호감도
@@ -178,7 +221,7 @@ public enum DialogType
         OtherPosition, //이전 대화의 캐릭터의 위치
         SkipNum, //스킵 했을 때 넘어가는 번호
         NextNum, //다음으로 넘어갈 번호
-        SelectText
+        SelectText //선택지 (개수 따라)
 }
 
 public enum DialogPosition
---
+++ Assets/02Script/UI/Chat/SelectBtn.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SelectBtn : MonoBehaviour
+{
+    public static Action<int> OnSelect; //선택한 번호
+
+    private int mySelectNum; //본인의 번호 (챕터에서, 넘버)
+    [SerializeField] private TextMeshProUGUI selectTexts; //선택지 대화
+
+    public void SetSelect(string text, int thisNum) //대화 내용, 선택지 번호
+    {
+        selectTexts.text = text;
+        mySelectNum = thisNum;
+    }
+
+    public void ClcikSelect() //선택 버튼 누를 때
+    {
+        OnSelect?.Invoke(mySelectNum + 1/*0부터 시작하니*/);
+    }
+}
