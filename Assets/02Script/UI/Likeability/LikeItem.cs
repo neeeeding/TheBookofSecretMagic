3aa47072665c9b9e31f4ea5c3cb1b0afcdc8c9b9
neeeeding <abc01096780866@gmail.com>
2025-04-15 18:55:37 +0900

    Add : LikeItem

--- Assets/02Script/UI/Likeability/LikeItem.cs
+++ Assets/02Script/UI/Likeability/LikeItem.cs
@@ -5,19 +5,27 @@ using UnityEngine.UI;
 
 public class LikeItem : MonoBehaviour
 {
+    [SerializeField]private ItemSO loveItem; //아이템 종류
+
+    private string savePath; //저장 경로
     private Image itemImage; //아이템 이미지
-    private int colorCount; //단계
+    private int colorCount; //(클릭) 단계
 
     private void Awake()
     {
         itemImage = GetComponent<Image>();
-        if (colorCount <= 0 || colorCount > 3)
-        {
-            colorCount = 1;
-        }
+    }
+    
+    public void SettingItem(ItemSO itemSO) //아이템 세팅
+    {
+        loveItem = itemSO;
+        itemImage.sprite = loveItem.itemImage;
+        savePath = $"{loveItem.itemType}likeItem";
+        colorCount = PlayerPrefs.GetInt(savePath) - 1; //더해주니까.
+        ChangeColorLove();
     }
 
-    public void  ClcikBtn()
+    public void  ChangeColorLove() //아이템 눌러 색 바꾸기
     {
         colorCount++;
         if(colorCount <= 0 || colorCount > 3)
@@ -32,5 +40,6 @@ public class LikeItem : MonoBehaviour
             3 => Color.red,
             _ => Color.white
         };
+        PlayerPrefs.SetInt(savePath, colorCount);
     }
 }
