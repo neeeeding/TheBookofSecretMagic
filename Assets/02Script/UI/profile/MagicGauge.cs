fcda30fb8bac7c18b4cfb65efeb5c3d065d720e7
neeeeding <abc01096780866@gmail.com>
2025-04-24 18:43:20 +0900

    Fix : MagicGauge

--- Assets/02Script/UI/profile/MagicGauge.cs
+++ Assets/02Script/UI/profile/MagicGauge.cs
@@ -16,35 +16,20 @@ public class MagicGauge : MonoBehaviour
 
     private void OnEnable()
     {
-        LoadCard.OnLoad += LoadStat;
-        if(stat !=  null)
+        if(GameManager.Instance.isStart)
         {
             LoadSlider();
-        }
-        else if(GameManager.Instance.isStart)
-        {
-            LoadStat();
         }
     }
 
     private void LoadSlider()
     {
+        stat = GameManager.Instance.PlayerStat;
         potionSlider.value = stat.potionMagic;
         copySlider.value = stat.copyMagic;
         waterSlider.value = stat.waterMagic;
         fireSlider.value = stat.fireMagic;
         healSlider.value = stat.healMagic;
         blackSlider.value = stat.blackMagic;
-    }
-
-    private void LoadStat()
-    {
-        stat = GameManager.Instance.PlayerStat;
-        LoadSlider();
-    }
-
-    private void OnDisable()
-    {
-        LoadCard.OnLoad -= LoadStat;
     }
 }
