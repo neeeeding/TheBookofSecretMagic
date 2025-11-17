822de9511c3f32a9dba1f1edea9833426db06812
neeeeding <abc01096780866@gmail.com>
2025-11-17 19:17:55 +0900

    Fix : cinemachine camera

--- Assets/02Script/UI/Map/MapTeleportation.cs
+++ Assets/02Script/UI/Map/MapTeleportation.cs
@@ -21,7 +21,7 @@ namespace _02Script.UI.Map
         
         public void TeleportationPos()
         {
-            Vector2 mapSize = imageRect.rect.size; //맵 사이즈
+            Vector2 mapSize = imageRect.rect.size/2; //맵 사이즈
             Vector2 mapPosition = imageRect.position; //맵 위치
             
             Camera uiCam = canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : canvas.worldCamera;
@@ -29,11 +29,7 @@ namespace _02Script.UI.Map
             RectTransformUtility.ScreenPointToLocalPointInRectangle(
                 imageRect, Input.mousePosition, uiCam, out Vector2 mapPos);
             
-            // mapPos = new Vector3(
-            //     ((localPoint.x - (mapSize.x / 2)) + mapPosition.x),
-            //     ((localPoint.y - (mapSize.y / 2)) + mapPosition.y), 0);
-            
-            ConvertRawImageToWorldPos(mapPos, out Vector3 playerPos);
+            ConvertRawImageToWorldPos(mapPos + mapSize, out Vector3 playerPos);
             
             GameManager.Instance.Player.transform.position = playerPos;
         }
@@ -46,10 +42,7 @@ namespace _02Script.UI.Map
             print(worldPos);
             
             bool success = RectTransformUtility.ScreenPointToWorldPointInRectangle(imageRect, mapPos, mainCamera, out Vector3 worldPoint);
-            //bool success = RectTransformUtility.ScreenPointToWorldPointInRectangle(imageRect, mapPos, mainCamera, out Vector2 worldPoint);
-            //Vector3 worldPoint = RectTransformUtility.WorldToScreenPoint(mainCamera, mapPos);
-            //Vector3 worldPoint = mainCamera.ScreenToWorldPoint(worldPos);
-            //worldPoint = mainCamera.ScreenToWorldPoint(worldPoint);
+
             print(worldPoint);
             worldPoint.z = 0;
             playerPos = worldPoint;
